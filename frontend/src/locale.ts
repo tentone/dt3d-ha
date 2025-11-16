fa6a6d07972310e739decf6ba35a19de9ba61024
Jose Ferrão <tentone@outlook.com>
2025-11-16 10:08:59 +0000

    Derp

--- frontend/src/locale.ts
+++ frontend/src/locale.ts
@@ -1,11 +1,52 @@
 export class Locale {
+    /**
+     * Translations storage
+     */
     public translations: { [key: string]: { [key: string]: string } } = {};
 
+    /**
+     * Locale selector
+     */
+    public current: string = null;
 
+    /**
+     * Set the current locale
+     * 
+     * @param locale - Locale code 
+     */
+    public set(locale: string) {
+        if (!this.translations[locale]) {
+            throw new Error(`Locale ${locale} not loaded`);
+        }
 
+        this.current = locale;
+    }
+
+    /**
+     * Get the translation for a given key
+     *
+     * @param key - Translation key
+     * @returns Translated string
+     */
+    public get(key: string): string {
+        if (!this.current) {
+            throw new Error("No locale set");
+        }
+        return this.translations[this.current][key] || key;
+    }
+
+    /**
+     * 
+     * @param locale - Locale code
+     * @param file - File name to load translations from
+     */
     public async loadTranslations(locale: string, file: string) {
         const response = await fetch(file);
         const data = await response.text();
         this.translations[locale] = JSON.parse(data);
+
+        if (!this.current) {
+            this.current = locale;
+        }
     }
 }
