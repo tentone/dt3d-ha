616d7fecd7b7031323adb82ca10b205f7136c95e
Jose Ferrão <joseferrao@outlook.com>
2026-07-22 19:50:47 +0100

    Fix generic entity icon

--- frontend/src/objects/entity-generic.ts
+++ frontend/src/objects/entity-generic.ts
@@ -1,20 +1,35 @@
+import { Color } from "three/src/math/Color.js";
+import { resolveHaIconPath, DEFAULT_HA_ICON } from "../utils/icon-utils.js";
+import { EntityBinary } from "./entity-binary.js";
 import {EntityObject} from "./entity-object.js";
 import {CircleIconSprite} from "./helpers/circle-icon-sprite.js";
 import {CSSText} from "./helpers/css-text.js";
+import { IconSprite } from "./helpers/icon-sprite.js";
 
 /**
  * Generic entity implementation used to represent entities of type that are not supported or have no specific interaction.
  */
 export class EntityGeneric extends EntityObject {
-	public label: CSSText;
+	/**
+	 * Icon of the binary entity.
+	 */
+	private icon: IconSprite;
+
+	/**
+	 * Label of the entity.
+	 */
+	private label: CSSText;
+ 
 
 	public constructor(entityId: string, entity: any) {
 		super(entityId);
 
-		const icon = new CircleIconSprite(0x1e90ff, 0.4);
-		icon.internal = true;
-		icon.position.y = 0.2;
-		this.add(icon);
+		const iconPath = resolveHaIconPath(entity?.attributes?.icon, DEFAULT_HA_ICON);
+
+		this.icon = new IconSprite(iconPath, new Color(0x1e90ff), 0.64);
+		this.icon.internal = true;
+		this.icon.position.y = 0.32;
+		this.add(this.icon);
 
 		const friendlyName = this.friendlyName(entity);
 
@@ -29,5 +44,7 @@ export class EntityGeneric extends EntityObject {
 
 	protected updateFromEntity(entity: any): void {
 		this.label.setText(this.friendlyName(entity));
+
+		this.icon.setIcon(resolveHaIconPath(entity.attributes?.icon, DEFAULT_HA_ICON));
 	}
 }
